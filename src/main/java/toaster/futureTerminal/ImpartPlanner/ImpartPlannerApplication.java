151ecc6ca09217f911f637cde82b2901127f33b9
FutureTerminal <zyanw236@gmail.com>
2023-07-07 15:34:53 -0400

    Added Java Gradle for set up

---
+++ src/main/java/toaster/futureTerminal/ImpartPlanner/ImpartPlannerApplication.java
@@ -0,0 +1,13 @@
+package toaster.futureTerminal.ImpartPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpartPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpartPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toaster/futureTerminal/ImpartPlanner/ImpartPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package toaster.futureTerminal.ImpartPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpartPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
